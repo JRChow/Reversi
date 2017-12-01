01dcc17954113ca6a465dfa017b16452ef0ab691
Jerome <zjrtomjohn@hotmail.com>
2017-12-01 21:09:10 +0800

    separate two games

--- reversi.py
+++ reversi.py
@@ -125,75 +125,13 @@ class Game:
                     return self.utility(state, self.to_move(self.initial))
 
 
-class TicTacToe(Game):
-    """Play TicTacToe on an h x v board, with Max (first player) playing 'X'.
-    A state has the player to move, a cached utility, a list of moves in
-    the form of a list of (x, y) positions, and a board, in the form of
-    a dict of {(x, y): Player} entries, where Player is 'X' or 'O'."""
+class Reversi(Game):
+    """Reversi game."""
 
-    def __init__(self, h=3, v=3, k=3):
-        self.h = h
-        self.v = v
-        self.k = k
-        moves = [(x, y) for x in range(1, h + 1)
-                 for y in range(1, v + 1)]
-        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
-
-    def actions(self, state):
-        """Legal moves are any square not yet taken."""
-        return state.moves
-
-    def result(self, state, move):
-        if move not in state.moves:
-            return state  # Illegal move has no effect
-        board = state.board.copy()
-        board[move] = state.to_move
-        moves = list(state.moves)
-        moves.remove(move)
-        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
-                         utility=self.compute_utility(board, move, state.to_move),
-                         board=board, moves=moves)
-
-    def utility(self, state, player):
-        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        return state.utility if player == 'X' else -state.utility
-
-    def terminal_test(self, state):
-        """A state is terminal if it is won or there are no empty squares."""
-        return state.utility != 0 or len(state.moves) == 0
-
-    def display(self, state):
-        board = state.board
-        for x in range(1, self.h + 1):
-            for y in range(1, self.v + 1):
-                print(board.get((x, y), '.'), end=' ')
-            print()
-
-    def compute_utility(self, board, move, player):
-        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
-        if (self.k_in_row(board, move, player, (0, 1)) or
-                self.k_in_row(board, move, player, (1, 0)) or
-                self.k_in_row(board, move, player, (1, -1)) or
-                self.k_in_row(board, move, player, (1, 1))):
-            return +1 if player == 'X' else -1
-        else:
-            return 0
-
-    def k_in_row(self, board, move, player, delta_x_y):
-        """Return true if there is a line through move on board for player."""
-        (delta_x, delta_y) = delta_x_y
-        x, y = move
-        n = 0  # n is number of moves in row
-        while board.get((x, y)) == player:
-            n += 1
-            x, y = x + delta_x, y + delta_y
-        x, y = move
-        while board.get((x, y)) == player:
-            n += 1
-            x, y = x - delta_x, y - delta_y
-        n -= 1  # Because we counted move itself twice
-        return n >= self.k
-
-
-t_game = TicTacToe()
-t_game.play_game(query_player, alphabeta_player)
+    def __init__(self, height=8, width=8):
+        self.height = height
+        self.width = width
+        # TODO: figure out what k is
+        moves = [(x, y) for x in range(1, width+1)
+                        for y in range(1, height+1)]
+        self.initial
