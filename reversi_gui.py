7e9a02e6c2e92d8d85c6710472b1718827ddc307
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 18:58:38 +0800

    Try to play...

--- reversi_gui.py
+++ reversi_gui.py
@@ -1,5 +1,5 @@
 from tkinter import *
-# from reversi import *
+from reversi import *
 
 
 class Board:
@@ -13,7 +13,7 @@ class Board:
                     button = Button(frame, text=".", bg="green")
                     position = (col, row)  # (x, y)
                     button.bind("<Button-1>", lambda event,
-                                arg=position: self.click(event, arg))
+                                arg=position: query_player(event, arg))
                     button.grid(row=row, column=col)
                     self.buttons[position] = button
                 if col == 0 and row > 0:
@@ -23,12 +23,16 @@ class Board:
                     label = Label(frame, text=chr(col + 96))
                     label.grid(row=row, column=col)
 
-    def click(self, event, arg):
-        print("clicked!")
-        print(arg)
-        self.buttons[arg].configure(bg="black")
+    # def click(self, event, arg):
+    #     print("clicked!")
+    #     print(arg)
+    #     self.buttons[arg].configure(bg="black")
 
 
 root = Tk()
 board = Board(root)
+
+game = Reversi()
+game.play_game(query_player, alphabeta_player)
+
 root.mainloop()
