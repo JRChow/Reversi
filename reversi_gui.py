0996191116a850689ded0b07ce1a015e2de17626
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 21:13:51 +0800

    correct module used

--- reversi_gui.py
+++ reversi_gui.py
@@ -37,7 +37,7 @@ class Board:
         self.buttons[move].configure(bg="black")
         self.state = self.game.play_game(move, self.state)
         self.update()
-        print("NEW MOVE == " + str(self.game.alphabeta_player(self.state)))
+        print("NEW MOVE == " + str(reversi_logic.alphabeta_player(self.state)))
 
     def update(self):
         print("Update!!")
