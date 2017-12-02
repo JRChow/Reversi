8d010cbde370de07dfbd10d7f3cc6e7493b06ef6
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 18:31:22 +0800

    Record map of button and make functions to self

--- reversi_gui.py
+++ reversi_gui.py
@@ -1,29 +1,30 @@
 from tkinter import *
 
 
-def click(event, arg):
-    print("clicked!")
-    print(arg)
-
-
 class Board:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
+        buttons = {}
         for row in range(0, 8 + 1):  # TODO: remove hard-code
             for col in range(0, 8 + 1):  # TODO: remove hard-code
                 if col > 0 and row > 0:
                     button = Button(frame, text=".")
-                    position = {"x": col, "y": row}
+                    position = (col, row)  # (x, y)
                     button.bind("<Button-1>", lambda event,
-                                arg=position: click(event, arg))
+                                arg=position: self.click(event, arg))
                     button.grid(row=row, column=col)
+                    buttons[position] = button
                 if col == 0 and row > 0:
                     label = Label(frame, text=str(row))
                     label.grid(row=row, column=col)
                 if row == 0 and col > 0:
                     label = Label(frame, text=chr(col + 96))
                     label.grid(row=row, column=col)
+
+    def click(event, arg):
+        print("clicked!")
+        print(arg)
 
 
 root = Tk()
