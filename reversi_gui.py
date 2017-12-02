ec710c2296154dfe84db62991fcd7a79b1c17ba3
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 20:20:23 +0800

    Try to use duck-typing

--- reversi_gui.py
+++ reversi_gui.py
@@ -11,22 +11,18 @@ class Board:
             for col in range(0, 8 + 1):  # TODO: remove hard-code
                 if col > 0 and row > 0:
                     if (col, row) in initial_state.board:
-                        button = Button(frame, bg="black" if initial_state.board.get(
+                        piece = Button(frame, bg="black" if initial_state.board.get(
                             (col, row)) == 'B' else 'white', state=DISABLED)
-                        button.grid(row=row, column=col)
                     else:
-                        button = Button(frame, text=".", bg="green")
-                        position = (col, row)  # (x, y)
-                        button.bind("<Button-1>", lambda event,
-                                    arg=position: self.click(event, arg))
-                        button.grid(row=row, column=col)
-                        self.buttons[position] = button
+                        piece = Button(frame, text=".", bg="green")
+                        piece.bind("<Button-1>", lambda event,
+                                   arg=(col, row): self.click(event, arg))
+                        self.buttons[(col, row)] = piece
                 if col == 0 and row > 0:
-                    label = Label(frame, text=str(row))
-                    label.grid(row=row, column=col)
+                    piece = Label(frame, text=str(row))
                 if row == 0 and col > 0:
-                    label = Label(frame, text=chr(col + 96))
-                    label.grid(row=row, column=col)
+                    piece = Label(frame, text=chr(col + 96))
+                piece.grid(row=row, column=col)
 
     def click(self, event, arg):
         print("clicked!")
