e2b7fb119f71e039f98b3c2c946bc06256ea747b
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 22:28:11 +0800

    doesnt work... try refresh

--- reversi_gui.py
+++ reversi_gui.py
@@ -42,7 +42,7 @@ class Board:
         self.state = self.game.play_game(move, self.state)
         self.update()
 
-        time.sleep(5)
+        Tk.update()
 
         ai_move = reversi_logic.alphabeta_player(self.game, self.state)
         self.state = self.game.play_game(ai_move, self.state)
