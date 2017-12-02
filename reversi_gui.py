92eaee8aa96f8235741c308d439c7a2fc341c997
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 17:54:05 +0800

    Class based

--- reversi_gui.py
+++ reversi_gui.py
@@ -1,8 +1,17 @@
 from tkinter import *
 
+
+class Board:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        button = Button(frame, text="Hi")
+        button.pack()
+        # for row in range(1, 8 + 1):  # TODO: remove hard-code
+        #     for col in range(1, 8 + 1):  # TODO: remove hard-code
+        #         button = Button
+
+
 root = Tk()
-frame = Frame(root)
-frame.pack()
-button = Button(frame, text="Hi")
-button.pack()
+board = Board(root)
 root.mainloop()
