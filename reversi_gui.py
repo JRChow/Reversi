af86190423da534c7c8411b43110f231196a9242
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 20:29:18 +0800

    try to incorporate valid moves

--- reversi_gui.py
+++ reversi_gui.py
@@ -3,10 +3,13 @@ import reversi_logic
 
 
 class Board:
-    def __init__(self, master, initial_state):
+    def __init__(self, master, game):
         frame = Frame(master)
         frame.pack()
         self.buttons = {}
+        initial_state = game.initial
+        valid_moves = game.get_valid_moves(
+            initial_state.board, initial_state.to_move)
         for row in range(0, 8 + 1):  # TODO: remove hard-code
             for col in range(0, 8 + 1):  # TODO: remove hard-code
                 piece = Label(frame)
@@ -14,6 +17,10 @@ class Board:
                     if (col, row) in initial_state.board:
                         piece = Button(frame, bg="black" if initial_state.board.get(
                             (col, row)) == 'B' else 'white', state=DISABLED)
+                    elif (col, row) in valid_moves:
+                        piece = Button(frame, text=".", bg="lawn green")
+                        piece.bind("<Button-1>", lambda event,
+                                   arg=(col, row): self.click(event, arg))
                     else:
                         piece = Button(frame, text=".", bg="green")
                         piece.bind("<Button-1>", lambda event,
@@ -31,8 +38,10 @@ class Board:
         self.buttons[arg].configure(bg="black")
         return arg
 
+    # def update(self, state):
+
 
 root = Tk()
 game = reversi_logic.Reversi()
-board = Board(root, game.initial)
+board = Board(root, game)
 root.mainloop()
