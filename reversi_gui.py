6984b97d75766f04021c4743117512dc6746c852
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 18:01:20 +0800

    Change text on button

--- reversi_gui.py
+++ reversi_gui.py
@@ -5,9 +5,14 @@ class Board:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-        for row in range(1, 8 + 1):  # TODO: remove hard-code
-            for col in range(1, 8 + 1):  # TODO: remove hard-code
-                button = Button(frame, text="Hi")
+        for row in range(0, 8 + 1):  # TODO: remove hard-code
+            for col in range(0, 8 + 1):  # TODO: remove hard-code
+                if col > 0 and row > 0:
+                    button = Button(frame, text=".")
+                if col == 0:
+                    button = Button(frame, text=str(row))
+                if row == 0:
+                    button = Button(frame, text=str(col))
                 button.grid(row=row, column=col)
 
 
