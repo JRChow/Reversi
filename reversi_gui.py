220324e06d8807bd6b82425e6739ae2bb6d1986c
Jerome <zjrtomjohn@hotmail.com>
2017-12-02 18:33:28 +0800

    Make change color

--- reversi_gui.py
+++ reversi_gui.py
@@ -5,7 +5,7 @@ class Board:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-        buttons = {}
+        self.buttons = {}
         for row in range(0, 8 + 1):  # TODO: remove hard-code
             for col in range(0, 8 + 1):  # TODO: remove hard-code
                 if col > 0 and row > 0:
@@ -14,7 +14,7 @@ class Board:
                     button.bind("<Button-1>", lambda event,
                                 arg=position: self.click(event, arg))
                     button.grid(row=row, column=col)
-                    buttons[position] = button
+                    self.buttons[position] = button
                 if col == 0 and row > 0:
                     label = Label(frame, text=str(row))
                     label.grid(row=row, column=col)
@@ -25,6 +25,7 @@ class Board:
     def click(self, event, arg):
         print("clicked!")
         print(arg)
+        self.buttons[arg].configure(bg="black")
 
 
 root = Tk()
